e6c2154e4bb6133274ef1b19db2d1e8c8d216699
FDR-96 <roldanfede96@gmail.com>
2024-12-08 20:52:51 -0300

    intentando

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
-
 set -e
 
 sleep 3 # Sleep 3 seconds for railway private URL to initialize
 
 # Crear la base de datos si no existe
 echo "Asegurando que la base de datos exista..."
-python app/utils/db_utils.py
+python -m app.utils.db_utils  # Ejecutar db_utils.py como módulo
 
 # Crear nuevas migraciones automáticamente si es necesario
 echo "Generando migraciones de Alembic..."
@@ -18,9 +17,13 @@ fi
 # Aplicar las migraciones a la base de datos
 echo "Aplicando migraciones a la base de datos..."
 alembic upgrade head
+
+# Establecer PYTHONPATH y ejecutar la aplicación
+echo "Iniciando la aplicación..."
 export PYTHONPATH=$(pwd)
 python -m app.main
 
 # Iniciar el servidor
 echo "Iniciando servidor..."
+PORT=${PORT:-8000}  # Definir el puerto si no está en las variables de entorno
 uvicorn app.main:app --host "0.0.0.0" --port $PORT
